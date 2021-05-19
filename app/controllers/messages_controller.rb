e0a0afdf683506f5bd34a9deaa8614379d09e9a1
Roman <vzdrizhni@gmail.com>
2021-05-19 20:42:38 +0500

    Add chat functionality

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -22,16 +22,10 @@ class MessagesController < ApplicationController
   # POST /messages or /messages.json
   def create
     @message = Message.new(message_params)
+    @message.user = current_user
 
-    respond_to do |format|
-      if @message.save
-        format.html { redirect_to @message, notice: "Message was successfully created." }
-        format.json { render :show, status: :created, location: @message }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
+    @message.save
+    redirect_to request.referrer
   end
 
   # PATCH/PUT /messages/1 or /messages/1.json
