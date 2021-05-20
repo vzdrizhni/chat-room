7d0c419552c6d7fc9e077a143d5aebcb6b801bf9
Roman <vzdrizhni@gmail.com>
2021-05-20 19:58:24 +0500

    Did some stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,17 @@ class ApplicationController < ActionController::Base
   def current_user
     return @current_user if @current_user.present?
 
-    if session[:user_id].present?
-      @current_user = User.find_by(id: session[:user_id])
+    if cookies[:user_id].present?
+      @current_user = User.find_by(id: cookies[:user_id])
     else
       @current_user = User.generate
-      session[:user_id] = @current_user.id
+      cookies[:user_id] = @current_user.id
       @current_user
     end
 
     if @current_user.nil?
       @current_user = User.generate
-      session[:user_id] = @current_user.id
+      cookies[:user_id] = @current_user.id
       @current_user
     end
 
